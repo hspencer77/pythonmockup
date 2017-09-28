3d98aac0d8e54946572d7c5ddc26c7f1a9cec5da
Harold Spencer Jr <hspencer77@gmail.com>
2017-09-28 18:03:02 -0400

    Added Dokerfile

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/bin/env python
-import time
-
-if __name__ == "__main__":
-    if str(time.time()).endswith('1'):
-        print "Time ends with a 1"
-    else:
-        print "Time ends with something else.."
